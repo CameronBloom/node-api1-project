ce5963be8e5b7db04c9817640f7c3bd93efb7d69
Cameron <cameron.morgan@skiff.com>
2023-07-25 13:03:48 -0400

    Implemented 'nodemon' and catch-all endpoint.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+// if no other end points match
+server.use("*", (req, res) => {
+  // send back a piece of JSON
+  res.status(404).json({
+    message: "error"
+
+  })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
