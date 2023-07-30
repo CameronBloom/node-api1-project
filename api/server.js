27cca9bef9846740b23919c828f95e6d8360e558
Cameron <cameron.morgan@skiff.com>
2023-07-30 09:54:58 -0400

    Implemented PUT functionality.

--- api/server.js
+++ api/server.js
@@ -6,17 +6,55 @@ const UserModel = require("./users/model")
 const server = express()
 server.use(express.json())
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const lookupUser = await UserModel.findById(req.params.id)
+    if (!lookupUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "Please provide name and bio for the user"
+        })
+      } else {
+        const updatedUser = await UserModel.update(
+          req.params.id, 
+          req.body
+        )
+        res.status(200).json(updatedUser)
+      }
+    } 
+  } catch (err) {
+      res.status(500).json({
+        message: "error updating user",
+        error: err.message,
+        stack: err.stack
+      })
+    }
+  })
+
 server.delete("/api/users/:id", async (req, res) => {
-  const lookupUser = await UserModel.findById(req.params.id)
-  if (!lookupUser) {
-    res.status(404).json({
-      message: "The user with the specified ID does not exist"
-    })
-  } else {
-    const deletedUser = await UserModel.remove(lookupUser.id)
-    res.status(200).json(deletedUser)
+  try {
+    const lookupUser = await UserModel.findById(req.params.id)
+    if (!lookupUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      const deletedUser = await UserModel.remove(lookupUser.id)
+      res.status(200).json(deletedUser)
+    } 
+  } catch (err) {
+      res.status(500).json({
+        message: "error deleting user",
+        error: err.message,
+        stack: err.stack
+      })
+    }
   }
-})
+)
 
 
 server.post("/api/users", (req, res) => {
